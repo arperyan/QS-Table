eb61a74815ffe0f2d41d501f44a38285ced7cac1
Mark Fereday <markfereday@hotmail.com>
2017-10-04 17:26:01 +0100

    Update defaults.js

---
+++ defaults.js
@@ -0,0 +1,27 @@
+define([], function(){
+  return {
+    extensionMeta: {
+      name : 'Calibrate Table',
+      description : 'Calibrate Table',
+      icon : 'table',
+      type : 'visualization',
+      version: '0.1.0',
+      author: 'Mark Fereday',
+      dependencies: {
+        'qlik-sense': '>=3.0.x'
+      },
+      email: 'markfereday@fereday.com',
+      homepage: 'fereday.com',
+      isLibraryItem: true,
+      isThirdParty: true,
+      keywords: 'qlik-sense, visualization',
+      license: '2017 © CC Ltd. - All rights reserved.',
+      preview: 'ccTable.png',
+      previewIconURL: '../extensions/ccTableV2/ccTable.png',
+      repository: '',
+      tc: '',
+      template: '',
+      templateIconClassName: ''
+    }
+  }
+})
